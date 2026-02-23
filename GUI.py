663a98c6e02dbbc8a96d1c05d64117b83a73c3c9
GabrielBlanc0o <gtbvacation@outlook.es>
2026-02-23 16:05:42 -0500

    Update tk user interface

--- GUI.py
+++ GUI.py
@@ -16,10 +16,21 @@ class interfaz(tk.Frame):
     def init_ui(self):
         """Aqui colocariamos los widgets."""
         self.parent.title("MicroSaas-Sentinel")
+        
+        titulo_principal = tk.Label(text="Business Health Status", font=("Helvetica", 20, "bold"), bg= "#1e1e2e",fg="#ffffff")
+        titulo_principal.pack(pady=40,  anchor="w",padx=150)
+        
+        self.barra_menu_literal = tk.Frame (self.parent,bg="#2b2b3b", width=300)
+        self.barra_menu_literal.pack(side="left", fill="y")
+        self.barra_menu_literal.pack_propagate(False)
+        
+        #texto_prueba = tk.Label(self.parent, text = "Hola")
+        #texto_prueba.pack()
 
 if __name__ == "__main__":
     ROOT = tk.Tk()
     ROOT.geometry("800x600")
+    ROOT.configure(bg="#1e1e2e")
     APP = interfaz(parent=ROOT)
     APP.mainloop()
     ROOT.destroy()
